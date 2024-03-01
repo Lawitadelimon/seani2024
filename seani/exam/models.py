2a726051961427d83a67bed6d2bb853ba5770e9b
Lawitadelimon <lau72abrvjua@gmail.com>
2024-02-29 23:37:36 -0600

    05 Ultimo commit

--- seani/exam/models.py
+++ seani/exam/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from career.models import Career
+from library.models import Module
 
 # Create your models here.
 
@@ -35,3 +38,51 @@ class Stage(models.Model):
     class Meta:
         verbose_name = "etapa"
         verbose_name_plural = "etapas"
+        
+class Exam(models.Model):
+    user = models.OneToOneField(
+                User, 
+                on_delete = models.CASCADE,
+                verbose_name = "Usuario"
+                )
+    stage = models.ForeignKey(
+                Stage,
+                on_delete = models.CASCADE,
+                verbose_name = "Etapa"
+                )
+    career = models.ForeignKey(
+            Career,
+                on_delete = models.CASCADE,
+                verbose_name = "Carrera"
+                )
+    modules = models.ManyToManyField(
+                Module,
+                through='ExamModule',
+                verbose_name = "Modulos"
+                )
+    score = models.FloatField(
+                verbose_name = "Calificacion",
+                default = 0.0
+                )
+    
+class ExamModule(models.Model):
+    exam = models.ForeignKey(
+                Exam,
+                on_delete = models.CASCADE,
+                verbose_name = "Examen"
+                )
+    module = models.ForeignKey(
+                Module,
+                on_delete = models.CASCADE,
+                verbose_name = "Modulo"
+                )
+    active = models.BooleanField(
+                verbose_name = "Activo",
+                default = True
+                )
+    
+    score = models.FloatField(
+                verbose_name = "Calificacion",
+                default = 0.0
+                )
+
