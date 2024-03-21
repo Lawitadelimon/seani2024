88caf5d8a70eec873419cd770ae913cdef894d4e
Lawitadelimon <lau72abrvjua@gmail.com>
2024-03-21 12:43:55 -0600

    08 Implements compute of scores

--- seani/seani/settings.py
+++ seani/seani/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -135,7 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL ='/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
-LOGIN_REDIRECT_URL = '/media/'
+LOGIN_REDIRECT_URL = '/exam'
 LOGOUT_REDIRECT_URL = '/'
 
 
--- seani/seani/urls.py
+++ seani/seani/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('exam/', include('exam.urls')),
     path('', include('home.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')), 
 ]
